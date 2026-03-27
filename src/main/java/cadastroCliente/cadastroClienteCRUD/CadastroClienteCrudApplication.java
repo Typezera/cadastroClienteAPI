874b46aca1019824faf793ef29aeb21b2cc43c6e
Typezera <willianbardela2131@hotmail.com>
2026-03-27 16:37:27 -0300

    first commit

---
+++ src/main/java/cadastroCliente/cadastroClienteCRUD/CadastroClienteCrudApplication.java
@@ -0,0 +1,13 @@
+package cadastroCliente.cadastroClienteCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroClienteCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroClienteCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cadastroCliente/cadastroClienteCRUD/CadastroClienteCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package cadastroCliente.cadastroClienteCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClienteCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
